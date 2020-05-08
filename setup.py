58218bb7f2a8a8ece23d984674f02ad657d0315d
jingjiang6 <jingjiang6@k002.cms.msina.yf.sinanode.com>
2020-05-08 16:13:32 +0800

    增加版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="concurrent_http_client",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "pycurl",
